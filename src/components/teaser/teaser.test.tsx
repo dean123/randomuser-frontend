e83f41a98a8b4ca23680239b4a2b34797a792e51
Dean Jürges <deanjuerges@posteo.de>
2023-02-25 11:02:00 +0100

    Added user model faker factory

--- src/components/teaser/teaser.test.tsx
+++ src/components/teaser/teaser.test.tsx
@@ -1,37 +1,17 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import { Teaser } from './teaser';
+import { getUser } from '../../factories/user-factory';
 
 test('renders full name', () => {
+  const user = getUser();
   render(
     <Teaser
-      onRemove={() => console.log('on remove')}
-      user={{
-        name: {
-          first: 'Test',
-          last: 'Tester',
-        },
-        gender: 'female',
-        dob: {
-          age: 31,
-          date: '1990-20-11',
-        },
-        picture: {
-          thumbnail: '',
-          medium: '',
-          large: '',
-        },
-        phone: '123',
-        nat: 'DE',
-        location: {
-          city: 'Berlin',
-          country: 'Germany',
-        },
-      }
-    } />
+      onRemove={() => {}}
+      user={user}
+    />
   );
 
-  const nameElement = screen.getByText(/Test Tester/i);
+  const nameElement = screen.getByText(user.name.first + ' ' + user.name.last);
   expect(nameElement).toBeInTheDocument();
-
 });
---
+++ src/factories/user-factory.ts
@@ -0,0 +1,38 @@
+import { faker } from '@faker-js/faker';
+import { User } from '../models/user';
+
+export function getUser(): User {
+  return {
+    name: {
+      first: faker.name.firstName(),
+      last: faker.name.lastName(),
+    },
+    gender: faker.name.gender(),
+    dob: {
+      age: faker.datatype.number({
+        min: 1,
+        max: 99,
+      }),
+      date: '',
+    },
+    picture: {
+      thumbnail: '',
+      medium: '',
+      large: '',
+    },
+    phone: faker.phone.number(),
+    nat: faker.address.countryCode(),
+    location: {
+      city: faker.address.city(),
+      country: faker.address.country(),
+    },
+  };
+}
+
+export function getUsers(amount: number): User[] {
+  const users = [];
+  for (let i = 0; i < amount; i++) {
+    users.push(getUser());
+  }
+  return users;
+}
