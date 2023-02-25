ef19da9f9b982938375a44fedcf610aacde4299c
Dean Jürges <deanjuerges@posteo.de>
2023-02-25 09:55:40 +0100

    Implemented handling of adding more users via button

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,41 @@ import { User } from './models/user';
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
+  const [addAmount, setAddAmount] = useState<number>(1);
 
-  const loadUsers = useCallback(async () => {
-    const data = await getRandomUsers();
-    setUsers(data);
+  const loadUsers = useCallback(async (amount: number) => {
+    const data = await getRandomUsers(amount);
+    setUsers((current: User[]) => [...current, ...data]);
   }, [])
 
+  const handleAdd = useCallback(() => {
+    loadUsers(addAmount);
+    setAddAmount((current: number) => current + 1);
+  }, [loadUsers, addAmount])
+
   useEffect(() => {
-    loadUsers();
+    loadUsers(20);
   }, [loadUsers])
 
   return (
-    <List>
-      {users.map((item: User) =>
-        <Teaser
-          key={item.cell}
-          user={item}
-        />
-      )}
-    </List>
+    <main>
+      <section>
+        <h1>Displaying {users.length} random users</h1>
+      </section>
+      <List>
+        {users.map((item: User) =>
+          <Teaser
+            key={item.cell}
+            user={item}
+          />
+        )}
+      </List>
+      <section>
+        <button onClick={handleAdd}>
+          Add {addAmount} More
+        </button>
+      </section>
+    </main>
   );
 }
 
--- src/lib/randomuser.ts
+++ src/lib/randomuser.ts
@@ -1,7 +1,7 @@
 import { User } from '../models/user';
 
-export default async function getRandomUsers(): Promise<User[]> {
-  const response = await fetch('https://randomuser.me/api?results=20');
+export default async function getRandomUsers(amount: number): Promise<User[]> {
+  const response = await fetch(`https://randomuser.me/api?results=${amount}`);
 
   if (response.ok) {
     const data = await response.json();
